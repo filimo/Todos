2a74987b9102e7fc7c64c0550ef8fcdcf394624f
Viktor Kushnerov <filimo@gmail.com>
2019-07-12 16:52:39 +0300

    Initial Commit

---
+++ Todos/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Todos
+//
+//  Created by Viktor Kushnerov on 7/12/19.
+//  Copyright © 2019 Viktor Kushnerov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
